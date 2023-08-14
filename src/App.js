155c7aad60f2ce7816a56e8062517c606fce2a9b
Luis Martínez <lifm9816@gmail.com>
2023-08-14 13:21:23 -0600

    Creación de carpeta y archivos .js y css 'MiOrg', en el cual se agregan el título para mostrar el personal de la organización y el botón para ocultar o mostrar el formulario para crear un nuevo usuario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header/Header.js"
 import Formulario from "./components/Formulario/Formulario.js"
+import MiOrg from "./components/MiOrg"
 
 function App() {
   return (
     <div>
       <Header/> 
       <Formulario/>
+      <MiOrg/>
     </div>
   );
 }
---
+++ src/components/MiOrg/index.js
@@ -0,0 +1,11 @@
+import "./MiOrg.css"
+
+const MiOrg = () =>
+{
+    return <section className = "orgSection">
+        <h3 className = "title">Mi organización</h3>
+        <img src ="/img/boton_add.png" alt = "add" />
+    </section>
+}
+
+export default MiOrg
