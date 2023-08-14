92701d3c2969c180477d240eb4da284d3f441c45
Luis Martínez <lifm9816@gmail.com>
2023-08-14 15:35:50 -0600

    Manipulación de la información e los inputs mediante de la función hook de react 'useState'

--- src/components/CampoTexto/CampoTexto.js
+++ src/components/CampoTexto/CampoTexto.js
@@ -1,11 +1,19 @@
+import {useState} from "react"
 import "./CampoTexto.css"
 
 const CampoTexto = (props) =>
 {
+
     const placeholderModificado = `${props.placeholder}...`
+
+    const manejarCambio = (e) =>
+    {
+        props.actualizarValor(e.target.value)
+    }
+
     return <div className = "campo-texto">
         <label htmlFor = {props.id} >{props.titulo}</label>
-        <input id = {props.id} placeholder = {placeholderModificado} required = {props.required}/>
+        <input id = {props.id} placeholder = {placeholderModificado} required = {props.required} value = {props.valor} onChange = {manejarCambio} />
     </div>
 }
 
--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,3 +1,4 @@
+import {useState} from "react"
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto/CampoTexto"
 import ListaOpciones from "../ListaOpciones/ListaOpciones"
@@ -5,20 +6,29 @@ import Boton from "../Boton/Boton.js"
 
 const Formulario = () =>
 {
+    const [nombre, actualizarNombre] = useState("");
+    const [puesto, actualizarPuesto] = useState("");
+    const [foto, actualizarFoto] = useState("");
     //Función que evitará que la página se recarge
     //cada que se llena la función del formulario
     const manejarEnvio = (evento) => 
     {
         evento.preventDefault();
         console.log("Manejar envío", evento);
+        let datosAEnviar = {
+            nombre: nombre,
+            puesto: puesto,
+            foto: foto
+        }
+        console.log(datosAEnviar);
     }
 
     return <section className = "formulario">
         <form onSubmit = {manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador</h2>
-            <CampoTexto id = "nombre" titulo = "Nombre" placeholder = "Ingresar nombre" required/>
-            <CampoTexto id = "puesto" titulo = "Puesto" placeholder = "Ingresar puesto" required/>
-            <CampoTexto id = "foto" titulo = "Foto" placeholder = "Ingresar enlace de foto" />
+            <CampoTexto id = "nombre" titulo = "Nombre" placeholder = "Ingresar nombre" required valor = {nombre} actualizarValor = {actualizarNombre} />
+            <CampoTexto id = "puesto" titulo = "Puesto" placeholder = "Ingresar puesto" required valor = {puesto} actualizarValor = {actualizarPuesto} />
+            <CampoTexto id = "foto" titulo = "Foto" placeholder = "Ingresar enlace de foto" required valor = {foto} actualizarValor = {actualizarFoto} />
             <ListaOpciones id = "opciones" placeholder = "Seleccionar equipo" required/>
             <Boton texto = "Crear"/>
         </form>
